e60982f7280dbcb3a0e9001f17af03c9973d1b94
Temiloluwa Ojo <themmyloluwaa@gmail.com>
2019-08-07 15:02:32 +0100

    added navigation header

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from "react";
 import CreateLinks from "./CreateLinks";
+import LinkList from "./LinkList";
+import Header from "./Header";
+import { Switch, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
-    return <CreateLinks />
+    return (
+      <div className="center w85">
+        <Header />
+        <div className="ph3 pv1 background-gray">
+          <Switch>
+            <Route exact path="/" component={LinkList} />
+            <Route exact path="/create" component={CreateLinks} />
+          </Switch>
+        </div>
+      </div>
+    );
   }
 }
 export default App;
